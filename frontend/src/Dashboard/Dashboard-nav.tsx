6a63f16847d6450182eeb2334359f9f941a2ec0e
yashchaud <yashchaudhari908@gmail.com>
2024-11-13 17:36:54 +0530

    gvg'

--- frontend/src/Dashboard/Dashboard-nav.tsx
+++ frontend/src/Dashboard/Dashboard-nav.tsx
@@ -1,4 +1,3 @@
-// DashboardNav.tsx
 import { Link, useLocation } from "react-router-dom";
 import { cn } from "@/lib/utils";
 import { NavItem } from "@/types";
@@ -16,7 +15,7 @@ import {
   AccordionTrigger,
   AccordionContent,
 } from "@/components/ui/accordion";
-import { Icons } from "./Icon"; // Ensure this is correctly imported and includes chevronDown
+import { Icons } from "./Icon";
 
 interface DashboardNavProps {
   items: NavItem[];
@@ -38,71 +37,107 @@ export function DashboardNav({
 
   return (
     <nav className="grid items-start gap-2">
-      <TooltipProvider>
+      <TooltipProvider delayDuration={0}>
         <Accordion type="multiple" className="w-full">
           {items.map((item, index) => {
             const Icon = Icons[item.icon || "arrowRight"];
             const isActive = location.pathname === item.href;
             const hasChildren = item.children && item.children.length > 0;
 
-            // Retrieve ChevronDown Icon
-            const ChevronDownIcon = Icons["chevronDown"];
-
             if (hasChildren) {
               return (
                 <AccordionItem value={item.title} key={index}>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <AccordionTrigger
-                        className={cn(
-                          "flex items-center justify-between pl-2 pr-2 w-full gap-2 overflow-hidden rounded-md py-2 text-sm font-medium hover:bg-secondary hover:text-iconActive",
-                          isActive ? "bg-nav text-black" : "transparent",
-                          item.disabled && "cursor-not-allowed opacity-80"
-                        )}
-                        disabled={item.disabled}
-                      >
-                        <div className="flex items-center gap-2">
-                          {Icon && (
-                            <Icon
-                              className={`ml-3 size-5 flex-none ${
-                                isActive
-                                  ? "text-iconActive text-black"
-                                  : "text-iconActive"
-                              }`}
-                            />
+                  {isMinimized && !isMobileNav ? (
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <AccordionTrigger
+                          className={cn(
+                            "flex items-center justify-between pl-2 pr-2 w-full gap-2 overflow-hidden rounded-md py-2 text-sm font-medium hover:bg-secondary hover:text-iconActive",
+                            isActive ? "bg-nav text-black" : "transparent",
+                            item.disabled && "cursor-not-allowed opacity-80"
                           )}
-                          {isMobileNav || (!isMinimized && !isMobileNav) ? (
-                            <span className="mr-2 truncate dark:text-[#e6e6e7] text-black">
-                              {item.title}
-                            </span>
-                          ) : null}
-                        </div>
-                        {/* Chevron Icon for Accordion */}
-                        {/* {ChevronDownIcon && (
-                          <ChevronDownIcon
-                            className={cn(
-                              "size-4 transition-transform",
-                              "group-data-[state=open]:rotate-180"
+                          disabled={item.disabled}
+                        >
+                          <div className="flex items-center gap-2">
+                            {Icon && (
+                              <Icon
+                                className={`ml-3 size-5 flex-none ${
+                                  isActive
+                                    ? "text-iconActive text-black"
+                                    : "text-iconActive"
+                                }`}
+                              />
                             )}
-                          />
-                        )} */}
-                      </AccordionTrigger>
-                    </TooltipTrigger>
-                    <TooltipContent
-                      align="center"
-                      side="right"
-                      sideOffset={8}
-                      className={!isMinimized ? "hidden" : "inline-block"}
+                          </div>
+                        </AccordionTrigger>
+                      </TooltipTrigger>
+                      <TooltipContent side="right" className="z-50">
+                        {item.title}
+                      </TooltipContent>
+                    </Tooltip>
+                  ) : (
+                    <AccordionTrigger
+                      className={cn(
+                        "flex items-center justify-between pl-2 pr-2 w-full gap-2 overflow-hidden rounded-md py-2 text-sm font-medium hover:bg-secondary hover:text-iconActive",
+                        isActive ? "bg-nav text-black" : "transparent",
+                        item.disabled && "cursor-not-allowed opacity-80"
+                      )}
+                      disabled={item.disabled}
                     >
-                      {item.title}
-                    </TooltipContent>
-                  </Tooltip>
-                  <AccordionContent className=" mt-2 space-y-1">
+                      <div className="flex items-center gap-2">
+                        {Icon && (
+                          <Icon
+                            className={`ml-3 size-5 flex-none ${
+                              isActive
+                                ? "text-iconActive text-black"
+                                : "text-iconActive"
+                            }`}
+                          />
+                        )}
+                        <span className="mr-2 truncate dark:text-[#e6e6e7] text-black">
+                          {item.title}
+                        </span>
+                      </div>
+                    </AccordionTrigger>
+                  )}
+                  <AccordionContent className="mt-2 space-y-1">
                     {item.children!.map((child, childIndex) => {
                       const ChildIcon = Icons[child.icon || "arrowRight"];
                       const isChildActive = location.pathname === child.href;
 
-                      return (
+                      return isMinimized && !isMobileNav ? (
+                        <Tooltip key={childIndex}>
+                          <TooltipTrigger asChild>
+                            <Link
+                              to={child.disabled ? "/" : child.href}
+                              className={cn(
+                                "flex items-center gap-2 overflow-hidden rounded-md py-1 text-sm font-medium hover:bg-secondary hover:text-accent-foreground",
+                                isChildActive
+                                  ? "bg-accent text-iconActive"
+                                  : "transparent",
+                                child.disabled &&
+                                  "cursor-not-allowed opacity-80"
+                              )}
+                              onClick={() => {
+                                if (setOpen) setOpen(false);
+                              }}
+                            >
+                              {ChildIcon && (
+                                <ChildIcon
+                                  className={`ml-6 size-4 flex-none ${
+                                    isChildActive
+                                      ? "text-iconActive"
+                                      : "text-iconDark"
+                                  }`}
+                                />
+                              )}
+                            </Link>
+                          </TooltipTrigger>
+                          <TooltipContent side="right" className="z-50">
+                            {child.title}
+                          </TooltipContent>
+                        </Tooltip>
+                      ) : (
                         <Link
                           key={childIndex}
                           to={child.disabled ? "/" : child.href}
@@ -126,11 +161,9 @@ export function DashboardNav({
                               }`}
                             />
                           )}
-                          {isMobileNav || (!isMinimized && !isMobileNav) ? (
-                            <span className="mr-2 truncate dark:text-[#e6e6e7] text-black">
-                              {child.title}
-                            </span>
-                          ) : null}
+                          <span className="mr-2 truncate dark:text-[#e6e6e7] text-black">
+                            {child.title}
+                          </span>
                         </Link>
                       );
                     })}
@@ -140,44 +173,61 @@ export function DashboardNav({
             } else {
               return (
                 <AccordionItem value={item.title} key={index}>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Link
-                        to={item.disabled ? "/" : item.href}
-                        className={cn(
-                          "flex items-center gap-2 overflow-hidden text-black rounded-md py-2 pl-2 text-sm font-medium hover:bg-secondary hover:text-accent-foreground",
-                          isActive ? "bg-accent text-black" : "transparent",
-                          item.disabled && "cursor-not-allowed opacity-80"
-                        )}
-                        onClick={() => {
-                          if (setOpen) setOpen(false);
-                        }}
-                      >
-                        {Icon && (
-                          <Icon
-                            className={`ml-3 size-5 flex-none ${
-                              isActive
-                                ? "text-accent text-black"
-                                : "text-accentlight"
-                            }`}
-                          />
-                        )}
-                        {isMobileNav || (!isMinimized && !isMobileNav) ? (
-                          <span className="mr-2 truncate dark:text-[#e6e6e7] text-black">
-                            {item.title}
-                          </span>
-                        ) : null}
-                      </Link>
-                    </TooltipTrigger>
-                    <TooltipContent
-                      align="center"
-                      side="right"
-                      sideOffset={8}
-                      className={!isMinimized ? "hidden" : "inline-block"}
+                  {isMinimized && !isMobileNav ? (
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Link
+                          to={item.disabled ? "/" : item.href}
+                          className={cn(
+                            "flex items-center gap-2 overflow-hidden text-black rounded-md py-2 pl-2 text-sm font-medium hover:bg-secondary hover:text-accent-foreground",
+                            isActive ? "bg-accent text-black" : "transparent",
+                            item.disabled && "cursor-not-allowed opacity-80"
+                          )}
+                          onClick={() => {
+                            if (setOpen) setOpen(false);
+                          }}
+                        >
+                          {Icon && (
+                            <Icon
+                              className={`ml-3 size-5 flex-none ${
+                                isActive
+                                  ? "text-accent text-black"
+                                  : "text-accentlight"
+                              }`}
+                            />
+                          )}
+                        </Link>
+                      </TooltipTrigger>
+                      <TooltipContent side="right" className="z-50">
+                        {item.title}
+                      </TooltipContent>
+                    </Tooltip>
+                  ) : (
+                    <Link
+                      to={item.disabled ? "/" : item.href}
+                      className={cn(
+                        "flex items-center gap-2 overflow-hidden text-black rounded-md py-2 pl-2 text-sm font-medium hover:bg-secondary hover:text-accent-foreground",
+                        isActive ? "bg-accent text-black" : "transparent",
+                        item.disabled && "cursor-not-allowed opacity-80"
+                      )}
+                      onClick={() => {
+                        if (setOpen) setOpen(false);
+                      }}
                     >
-                      {item.title}
-                    </TooltipContent>
-                  </Tooltip>
+                      {Icon && (
+                        <Icon
+                          className={`ml-3 size-5 flex-none ${
+                            isActive
+                              ? "text-accent text-black"
+                              : "text-accentlight"
+                          }`}
+                        />
+                      )}
+                      <span className="mr-2 truncate dark:text-[#e6e6e7] text-black">
+                        {item.title}
+                      </span>
+                    </Link>
+                  )}
                 </AccordionItem>
               );
             }
