5ecc8dc2eecdd42d9463e8042d2ad65ea1ad61fc
Vipulkoli2000 <vipulkoli2323@gmail.com>
2024-11-19 18:08:20 +0530

    Removed Export

--- frontend/src/Components/Parameter/Dashboardreuse.tsx
+++ frontend/src/Components/Parameter/Dashboardreuse.tsx
@@ -211,17 +211,7 @@ export default function Dashboard({
                     ))}
                   </DropdownMenuContent>
                 </DropdownMenu>
-                <Button
-                  size="sm"
-                  variant="outline"
-                  className="h-8 gap-1"
-                  onClick={onExport}
-                >
-                  <File className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Export
-                  </span>
-                </Button>
+               
                 {/* <Button size="sm" className="h-8 gap-1" onClick={onAddProduct}>
                   <PlusCircle className="h-3.5 w-3.5" />
                   <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
--- frontend/src/Components/TestLinkMaster/Dashboardreuse.tsx
+++ frontend/src/Components/TestLinkMaster/Dashboardreuse.tsx
@@ -233,17 +233,7 @@ const Dashboard: React.FC<DashboardProps> = ({
                     ))}
                   </DropdownMenuContent>
                 </DropdownMenu>
-                <Button
-                  size="sm"
-                  variant="outline"
-                  className="h-8 gap-1"
-                  onClick={onExport}
-                >
-                  <File className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Export
-                  </span>
-                </Button>
+               
                 {/* <Button size="sm" className="h-8 gap-1" onClick={onAddProduct}>
                   <PlusCircle className="h-3.5 w-3.5" />
                   <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
